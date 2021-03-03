792e2b6df49f4e42628816da866656dceb89314a
matiasngarcia <mngarcia@3241.com.ar>
2021-03-02 21:10:18 -0300

    Practica Clase 3 - Incoporacion de menu de navegacion (ejemplo)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,39 +3,34 @@ import {
     MDBNavbar,
     MDBNavbarBrand,
     MDBNavbarToggler,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBNavbarLink
+    MDBNavbarLink,
+    MDBIcon
   } from 'mdb-react-ui-kit';
 
 function NavBar() {
-    return (
-        <MDBNavbar expand='lg' light bgColor='light'>
-          <MDBContainer fluid>
-            <MDBNavbarBrand href='#'>Tienda de algo</MDBNavbarBrand>
-            
-            <div className='collapse navbar-collapse' id='navbarNav'>
-              <MDBNavbarNav>
-                <MDBNavbarItem>
-                  <MDBNavbarLink active aria-current='page' href='#'>
-                    Home
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href='#'>Productos</MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href='#'>Login</MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href='#'>Registro</MDBNavbarLink>
-                </MDBNavbarItem>
-                
-              </MDBNavbarNav>
-            </div>
-          </MDBContainer>
-        </MDBNavbar>
-      );
+  return (
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>Tienda de Algo</MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          aria-controls='navbarNavAltMarkup'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+          <div className='navbar-nav'>
+            <MDBNavbarLink active href='#'>Productos</MDBNavbarLink>
+            <MDBNavbarLink href='#'>Envíos</MDBNavbarLink>
+            <MDBNavbarLink href='#'>Ingresar</MDBNavbarLink>
+            <MDBNavbarLink href='#'>Registrate</MDBNavbarLink>
+          </div>
+        </div>
+      </MDBContainer>
+    </MDBNavbar>
+  );
 }
 
 export default NavBar;
